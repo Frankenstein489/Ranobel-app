3bc60dd9b0aa6b44e8337977b1eea972416a3059
Frankenstein489 <pranayrai207@gmail.com>
2026-07-22 17:55:02 +0545

    minor changes added

--- novels.config.ts
+++ novels.config.ts
@@ -30,7 +30,7 @@ const novels: Novel[] = [
     description:
       "A man is stabbed by a robber on the run after pushing his coworker and his coworker’s new fiance out of the way. As he lays dying, bleeding on the ground, he hears a voice. This voice is strange and interprets his dying regret of being a virgin by giving him the [Great Sage] unique skill! Is he being made fun of ?!",
     genre: ["Comedy", "Fantasy", "Action", "Adventure", "Shounen"],
-    coverImage: "/covers/Kim.jpg",
+    coverImage: "/covers/slime.jpg",
     totalChapters: 413,
     contentFolder: "slime",
     type: "md",
@@ -42,7 +42,7 @@ const novels: Novel[] = [
     description:
       "Only I know the end of this world.One day our MC finds himself stuck in the world of his favorite webnovel. What does he do to survive? It is a world struck by catastrophe and danger all around.His edge? He knows the plot of the story to end. Because he was the sole reader that stuck with it. Read his story to see how he survives!",
     genre: ["Action, Adventure, Drama, Fantasy, Psychological"],
-    coverImage: "/covers/Rimuru.jpg",
+    coverImage: "/covers/Kim.jpg",
     totalChapters: 551,
     contentFolder: "omniscient",
     type: "md",
